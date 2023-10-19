b223771aafc27168e11b0beb0d64a18da0283df0
impossibleshadowstorm <impossibleshadowstorm@gmail.com>
2023-10-19 20:12:39 +0530

    minor bug fixes..

--- src/app/component/hero.js
+++ src/app/component/hero.js
@@ -24,12 +24,12 @@ export default function Hero() {
             {/* Btn */}
             <a
               href="#_"
-              class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-black transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
+              className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-black transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
             >
-              <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-[black] group-hover:h-full"></span>
-              <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+              <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-[black] group-hover:h-full"></span>
+              <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
                 <svg
-                  class="w-5 h-5 text-black"
+                  className="w-5 h-5 text-black"
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
@@ -43,9 +43,9 @@ export default function Hero() {
                   ></path>
                 </svg>
               </span>
-              <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+              <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
                 <svg
-                  class="w-5 h-5 text-[white]"
+                  className="w-5 h-5 text-[white]"
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
@@ -59,7 +59,7 @@ export default function Hero() {
                   ></path>
                 </svg>
               </span>
-              <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
+              <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">
                 Read About Us
               </span>
             </a>
--- src/app/component/services.js
+++ src/app/component/services.js
@@ -51,10 +51,10 @@ export default function Services() {
               </p>
               <a
                 href="#_"
-                class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white  hover:bg-white group w-40 xs:max-md:mt-16"
+                className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white  hover:bg-white group w-40 xs:max-md:mt-16"
               >
-                <span class="w-48 h-48 rounded rotate-[-40deg] bg-[#F5D856] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                <span class="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-black">
+                <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#F5D856] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-black">
                   See Details
                 </span>
               </a>
